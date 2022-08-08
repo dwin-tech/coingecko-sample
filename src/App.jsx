867826de13ca2ec278d4c3ddacbf704e284cd3ce
lilit99a <lilit.avetyan.99@bk.ru>
2022-08-08 14:37:26 +0400

    added folders and eslint

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import Footer from "./components/footer";
+
+function App() {
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
